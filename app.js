15860e14829d0f284f74ac57cad2f3ad302ffd2c
Abdul Samea <abdulsamea2@gmail.com>
2020-02-24 23:49:08 +0530

    add and read notes functionality done

--- app.js
+++ app.js
@@ -1,4 +1,96 @@
-const getNotes = require('./notes.js')
-const msg = getNotes()
+const notes = require('./notes.js')
+const validator = require('validator')
+const chalk = require('chalk')
+const yargs = require('yargs')
+const fs = require('fs')
+const path = require('path')
 
-console.log(msg);
+
+
+
+
+const successMsg = chalk.rgb(255,127,80).bold('Success for Sam on the way!')
+
+const title = yargs.argv.title
+var body = yargs.argv.body
+
+console.log(successMsg)
+console.log(validator.isEmail('abdul@fl.com'))
+
+// add a note command
+yargs.command({
+    command : 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type:'string'
+        },
+        body: {
+            describe: 'Note Content',
+            demandOption: true,
+            type:'string'
+        }
+    },
+    handler: function(){
+        notes.addNote(title, body);
+    }
+})
+
+
+// remove a note command
+yargs.command({
+    command : 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type:'string'
+        }
+    },
+    handler: function(){
+        console.log('removing a note...');
+        if(title != undefined){
+            fs.unlink(title + '.txt', function(){
+                console.log(title + ' deleted by nodejs')
+            });
+    }
+    }
+})
+
+// list all notes command
+yargs.command({
+    command : 'list',
+    describe: 'Listing out all notes in this app',
+    handler: function(){
+        console.log('All notes present are:');
+        fs.readdir('./', (err, files) => {
+            files.forEach(file => {
+                if(path.extname(file) == '.txt' ){
+                    console.log(file);
+                }
+            });
+          });
+    }
+})
+
+// read a note command
+yargs.command({
+    command : 'read',
+    describe: 'Read a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type:'string'
+        }
+    },
+    handler: function(){
+    notes.getNotes(title);
+    }
+})
+
+// console.log('y = ',yargs.argv)
+yargs.parse()
--- notes.js
+++ notes.js
@@ -1,5 +1,41 @@
-const getNotes = function(){
-    return 'Your notes...'
+const fs = require('fs')
+
+
+const getNotes = function(title){
+    try{
+        console.log('<<<<<<<<<<<<-------- reading the  note -------->>>>>>>>>>>>');
+        if(title != undefined){
+            fs.readFile('./'+ title + '.txt', 'utf8', function(err, contents) {
+                console.log(contents);
+            })
+
+    }
+    }
+    catch(e){
+        console.log(e)
+    }
 }
 
-module.exports = getNotes
+
+
+const addNote = function(title, body){
+
+    try{
+        console.log('adding a new note...');
+        if(title != undefined)
+        fs.appendFile(title  + '.txt', body + '\n', function(){
+            console.log(title + ' ceated by nodejs')
+        });
+
+    }
+
+    catch(e){
+        console.log(e);
+    }
+
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+}
